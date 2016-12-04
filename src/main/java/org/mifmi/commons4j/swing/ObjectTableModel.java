2687bea0af835d173e1771c60f7c665d710eb49d
mozq <git@mozq.net>
2016-12-04 18:01:25 +0900

    Update copyright headers.

--- src/main/java/org/mifmi/commons4j/swing/ObjectTableModel.java
+++ src/main/java/org/mifmi/commons4j/swing/ObjectTableModel.java
@@ -1,3 +1,11 @@
+/*!
+ * mifmi-commons4j
+ * https://github.com/mifmi/mifmi-commons4j
+ *
+ * Copyright (c) 2016 mifmi.org and other contributors
+ * Released under the MIT license
+ * https://opensource.org/licenses/MIT
+ */
 package org.mifmi.commons4j.swing;
 
 import java.util.ArrayList;
