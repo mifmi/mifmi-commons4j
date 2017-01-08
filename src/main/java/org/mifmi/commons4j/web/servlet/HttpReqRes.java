eb109b0efce027a1cfb6bc7d80aa8fa039584030
mozq <git@mozq.net>
2017-01-08 10:08:34 +0900

    Remove unused imports

--- src/main/java/org/mifmi/commons4j/web/servlet/HttpReqRes.java
+++ src/main/java/org/mifmi/commons4j/web/servlet/HttpReqRes.java
@@ -21,7 +21,6 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 
 import javax.servlet.ServletException;
