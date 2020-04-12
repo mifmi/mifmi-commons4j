35325e961b1e5a5b943762ea50a98ec2059b0408
mozq <git@mozq.net>
2020-04-11 20:54:42 -0400

    Add HTMLUtilz class

---
+++ src/test/java/test/org/mifmi/commons4j/web/util/HTMLUtilzTest.java
@@ -0,0 +1,115 @@
+/*!
+ * mifmi-commons4j
+ * https://github.com/mifmi/mifmi-commons4j
+ *
+ * Copyright (c) 2020 mifmi.org and other contributors
+ * Released under the MIT license
+ * https://opensource.org/licenses/MIT
+ */
+package test.org.mifmi.commons4j.web.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.mifmi.commons4j.web.util.HTMLUtilz;
+
+public class HTMLUtilzTest {
+	
+	@Test
+	public void testEscapeBasicHTML() throws Exception {
+		assertEquals(null, HTMLUtilz.escapeBasicHTML(null));
+		assertEquals("", HTMLUtilz.escapeBasicHTML(""));
+		
+		assertEquals("&amp;", HTMLUtilz.escapeBasicHTML("&"));
+		assertEquals("&lt;", HTMLUtilz.escapeBasicHTML("<"));
+		assertEquals("&gt;", HTMLUtilz.escapeBasicHTML(">"));
+		assertEquals("&quot;", HTMLUtilz.escapeBasicHTML("\""));
+		assertEquals("&apos;", HTMLUtilz.escapeBasicHTML("'"));
+		assertEquals("&#39;", HTMLUtilz.escapeBasicHTML("'", false, true));
+		
+		assertEquals(" ", HTMLUtilz.escapeBasicHTML(" "));
+		assertEquals("&nbsp;", HTMLUtilz.escapeBasicHTML(" ", true));
+		assertEquals("a", HTMLUtilz.escapeBasicHTML("a"));
+		
+		assertEquals("&amp;&amp;&amp;", HTMLUtilz.escapeBasicHTML("&&&"));
+		assertEquals("&amp;x&amp;x&amp;", HTMLUtilz.escapeBasicHTML("&x&x&"));
+		assertEquals("x&amp;x&amp;x&amp;x", HTMLUtilz.escapeBasicHTML("x&x&x&x"));
+	}
+
+	@Test
+	public void testUnescapeHTML5() throws Exception {
+		assertEquals(null, HTMLUtilz.unescapeHTML5(null));
+		assertEquals("", HTMLUtilz.unescapeHTML5(""));
+		
+		assertEquals("&", HTMLUtilz.unescapeHTML5("&amp;"));
+		assertEquals("<", HTMLUtilz.unescapeHTML5("&lt;"));
+		assertEquals(">", HTMLUtilz.unescapeHTML5("&gt;"));
+		assertEquals("\"", HTMLUtilz.unescapeHTML5("&quot;"));
+		assertEquals("'", HTMLUtilz.unescapeHTML5("&apos;"));
+		assertEquals(" ", HTMLUtilz.unescapeHTML5("&nbsp;"));
+		
+		assertEquals("\u00A9", HTMLUtilz.unescapeHTML5("&copy;"));
+		assertEquals("\u00A9", HTMLUtilz.unescapeHTML5("&COPY;"));
+		
+		assertEquals("\uD835\uDCB5", HTMLUtilz.unescapeHTML5("&Zscr;"));
+		assertEquals("\uD835\uDCCF", HTMLUtilz.unescapeHTML5("&zscr;"));
+		
+		assertEquals("a", HTMLUtilz.unescapeHTML5("&#97;"));
+		assertEquals("a", HTMLUtilz.unescapeHTML5("&#097;"));
+		assertEquals("a", HTMLUtilz.unescapeHTML5("&#x61;"));
+		assertEquals("a", HTMLUtilz.unescapeHTML5("&#x0061;"));
+		
+		assertEquals("&", HTMLUtilz.unescapeHTML5("&amp"));
+		assertEquals("a", HTMLUtilz.unescapeHTML5("&#97"));
+		assertEquals("a", HTMLUtilz.unescapeHTML5("&#097"));
+		assertEquals("a", HTMLUtilz.unescapeHTML5("&#x61"));
+		assertEquals("a", HTMLUtilz.unescapeHTML5("&#x0061"));
+		
+		assertEquals(" ", HTMLUtilz.unescapeHTML5(" "));
+		assertEquals("a", HTMLUtilz.unescapeHTML5("a"));
+		
+		assertEquals("&&&", HTMLUtilz.unescapeHTML5("&amp;&amp;&amp;"));
+		assertEquals("&x&x&", HTMLUtilz.unescapeHTML5("&amp;x&amp;x&amp;"));
+		assertEquals("x&x&x&x", HTMLUtilz.unescapeHTML5("x&amp;x&amp;x&amp;x"));
+		
+		assertEquals("aaa", HTMLUtilz.unescapeHTML5("&#97;&#97;&#97;"));
+		assertEquals("axaxa", HTMLUtilz.unescapeHTML5("&#97;x&#97;x&#97;"));
+		assertEquals("xaxaxax", HTMLUtilz.unescapeHTML5("x&#97;x&#97;x&#97;x"));
+		
+		assertEquals("aaa", HTMLUtilz.unescapeHTML5("&#x61;&#x61;&#x61;"));
+		assertEquals("axaxa", HTMLUtilz.unescapeHTML5("&#x61;x&#x61;x&#x61;"));
+		assertEquals("xaxaxax", HTMLUtilz.unescapeHTML5("x&#x61;x&#x61;x&#x61;x"));
+		
+		assertEquals("&&&", HTMLUtilz.unescapeHTML5("&amp&amp&amp"));
+		assertEquals("& & &", HTMLUtilz.unescapeHTML5("&amp &amp &amp"));
+		assertEquals(" & & & ", HTMLUtilz.unescapeHTML5(" &amp &amp &amp "));
+		
+		assertEquals("aaa", HTMLUtilz.unescapeHTML5("&#97&#97&#97"));
+		assertEquals("axaxa", HTMLUtilz.unescapeHTML5("&#97x&#97x&#97"));
+		assertEquals("xaxaxax", HTMLUtilz.unescapeHTML5("x&#97x&#97x&#97x"));
+		
+		assertEquals("aaa", HTMLUtilz.unescapeHTML5("&#x61&#x61&#x61"));
+		assertEquals("axaxa", HTMLUtilz.unescapeHTML5("&#x61x&#x61x&#x61"));
+		assertEquals("xaxaxax", HTMLUtilz.unescapeHTML5("x&#x61x&#x61x&#x61x"));
+		
+		assertEquals("&", HTMLUtilz.unescapeHTML5("&"));
+		assertEquals("&;", HTMLUtilz.unescapeHTML5("&;"));
+		assertEquals("&xxx;", HTMLUtilz.unescapeHTML5("&xxx;"));
+		assertEquals("&xxx", HTMLUtilz.unescapeHTML5("&xxx"));
+		assertEquals("&ampx", HTMLUtilz.unescapeHTML5("&ampx"));
+		
+		assertEquals("&#", HTMLUtilz.unescapeHTML5("&#"));
+		assertEquals("&#;", HTMLUtilz.unescapeHTML5("&#;"));
+		assertEquals("&#axx;", HTMLUtilz.unescapeHTML5("&#axx;"));
+		assertEquals("&#axx", HTMLUtilz.unescapeHTML5("&#axx"));
+		assertEquals("&#9999999999", HTMLUtilz.unescapeHTML5("&#9999999999"));
+		
+		assertEquals("&#x", HTMLUtilz.unescapeHTML5("&#x"));
+		assertEquals("&#x;", HTMLUtilz.unescapeHTML5("&#x;"));
+		assertEquals("&#xxx;", HTMLUtilz.unescapeHTML5("&#xxx;"));
+		assertEquals("&#xxx", HTMLUtilz.unescapeHTML5("&#xxx"));
+		assertEquals("&#x9999999999", HTMLUtilz.unescapeHTML5("&#x9999999999"));
+
+		assertEquals("x&#xxxx", HTMLUtilz.unescapeHTML5("x&#xxxx"));
+	}
+}
